3fb00bb13aa86567ca3892bd3a4fcdffade58081
MacolmTann <malcolmtan20@gmail.com>
2022-09-26 01:04:59 -0700

    Bio image background

--- index.php
+++ index.php
@@ -5,11 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="styles/reset.css">
-        <link rel="stylesheet" href="styles/style.css">
+        <?php include('partials/links.php'); ?>
     </head>
     <body>
         <?php include('partials/header.php'); ?>
