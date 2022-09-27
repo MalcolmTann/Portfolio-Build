41b2690741a5a4d129ad1253c1092b62b631dcc0
MacolmTann <malcolmtan20@gmail.com>
2022-09-27 01:31:07 -0700

    project page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Malcolm Tan</title>
+        <title>Malcolm Tan | Front-end Developer</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +10,11 @@
     <body>
         <?php include('partials/header.php'); ?>
         <main>
-            <?php include('partials/intro.php'); ?>
+            <?php include('partials/home/landing.php'); ?>
 
-            <?php include('partials/projects.php'); ?>
+            <?php include('partials/home/projects.php'); ?>
             
-            <?php include('partials/bio.php'); ?>
+            <?php include('partials/home/bio.php'); ?>
         </main> 
         <?php include('partials/footer.php'); ?>
     </body>
--- project-1.php
+++ project-1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Document</title>
+        <title>Malcolm Tan | [Project Name]</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +10,44 @@
     <body>
         <?php include('partials/header.php'); ?>
         <main>
-
+            <section class="proj-land">
+                <div class="col-1">
+                    <img src="images/malsathome.jpg">
+                </div>
+                <div class="col-2">
+                    <p>/ Bootstrap</p>
+                    <h1>Malsathome</h1>
+                    <p>A ficitional restaurant</p>
+                    <button class="btn">Live Site</button>
+                </div>
+            </section>
+            <section class="skills">
+                <div class="is-flex">
+                    <div class="col">
+                        <h4>Completed On</h4>
+                        <p>01/01/21</p>
+                    </div>
+                    <div class="col">
+                        <h4>Role</h4>
+                        <ul>
+                            <li>Developer</li>
+                            <li>Designer</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h4>Software</h4>
+                        <p>Visual Studio Code</p>
+                    </div>
+                </div>
+            </section>
+            <section class="overview">
+               <h2>Overview</h2>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente deleniti quas consequatur vel omnis perferendis laborum dolorem sint nesciunt.</p>
+            </section>
+            <section class="more-projects">
+                <button class="btn">Prev</button>
+                <button class="btn">Next</button>
+            </section>
         </main>
         <?php include('partials/footer.php'); ?>
     </body>
