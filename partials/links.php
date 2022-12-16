277f8d185718b6250f6dfde3cdf1085eeb6452a4
MacolmTann <malcolmtan20@gmail.com>
2022-12-16 13:48:00 -0800

    update minified css

--- partials/links.php
+++ partials/links.php
@@ -3,4 +3,4 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<link rel="stylesheet" href="../styles/css/main.css">
+<link rel="stylesheet" href="../styles/css/main-min.css">
