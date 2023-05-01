94b1305c03d9c245c19a5756d9c17d9bdd7e1d2f
MacolmTann <malcolmtan20@gmail.com>
2023-05-01 15:59:57 -0700

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,6 @@
 <header data-aos="fade" data-aos-delay="1500" data-aos-duration="2000">
     <div class="nav-container">
-        <a href="index.php" title="Home">
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve"><g><path d="M631.8,439l-0.3-112.2l-55,92.4h-19.5l-54.7-90V439h-40.6c0,0,46.3-187,25.3-187s10.5,0.1,10.5,0.1L587,439l48-183l10,1
-            l27.4,182H631.8z"/><path d="M654.4,274.2c-32.4-0.2-70.7,0-70.7,0L602,252c0,0,170,22,173,0c0-17,6,21.6,0,21.8c-6,0.2-69.4,0.4-69.4,0.4L680,439l0,0
-            C680,439,686.9,274.4,654.4,274.2z"/></g>    
-            </svg>
-        </a>
+        <a href="index.php" title="Home"><h2>Mal</h2></a>
         <button title="menu" class="gn-trig" data-open="0">
             <span class="burger"></span>
         </button>
